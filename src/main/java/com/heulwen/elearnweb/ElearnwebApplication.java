63239d8b983ddc1d7ddd054264a52ecb33864bf2
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2025-07-15 16:46:25 +0700

    Initial commit with source code

---
+++ src/main/java/com/heulwen/elearnweb/ElearnwebApplication.java
@@ -0,0 +1,13 @@
+package com.heulwen.elearnweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearnwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearnwebApplication.class, args);
+	}
+
+}
