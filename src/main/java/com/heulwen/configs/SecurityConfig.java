52b4c675e4d2ad113be41d2d4f8bf7d3c9c74260
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2025-08-20 19:38:07 +0700

    update cors

--- src/main/java/com/heulwen/configs/SecurityConfig.java
+++ src/main/java/com/heulwen/configs/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000/")); 
+        config.setAllowedOrigins(List.of("http://localhost:3000/", "https://englearn-frontend.onrender.com/")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
